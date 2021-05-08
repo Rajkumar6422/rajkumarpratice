93c25fd579f7a97ef980d88eab4dc88aa89adf78
Rajkumar6422 <83816107+Rajkumar6422@users.noreply.github.com>
2021-05-08 16:36:37 +0530

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=5
+
+
+# In[2]:
+
+
+print(a)
+
+
+# In[3]:
+
+
+b=10
+
+
+# In[4]:
+
+
+print(b)
+
+
+# In[5]:
+
+
+print(a+b)
+
+
+# In[6]:
+
+
+id(a)
+
+
+# In[7]:
+
+
+id(b)
+
+
+# In[9]:
+
+
+type(a)
+
+
+# In[24]:
+
+
+a=5.5
+
+
+# In[25]:
+
+
+b=5.6
+
+
+# In[26]:
+
+
+print(a*b)
+
+
+# In[27]:
+
+
+id(a*b)
+
+
+# In[30]:
+
+
+a="raj kumar"
+
+
+# In[31]:
+
+
+print(a)
+
+
+# In[32]:
+
+
+type(a)
+
+
+# In[33]:
+
+
+full_name="'raj kumar'"
+
+
+# In[34]:
+
+
+print(full_name)
+
+
+# In[35]:
+
+
+print(full_name.title())
+
+
+# In[36]:
+
+
+print(full_name.upper())
+
+
+# In[37]:
+
+
+print(full_name.lower())
+
+
+# In[38]:
+
+
+print("raj kumar")
+
+
+# In[39]:
+
+
+print("\t raj kumar")
+
+
+# In[40]:
+
+
+print("\n raj kumar")
+
+
+# In[41]:
+
+
+print("\n\t raj kumar")
+
+
+# In[42]:
+
+
+print("\t\n rajkumar")
+
+
+# In[45]:
+
+
+print("my friends: \n janaki ram \n datta \n shiva \n subbu")
+
+
+# In[51]:
+
+
+print("my friends:\n\t janaki ram\n\t datta\n\t shiva\n\t subbu")
+
+
+# In[55]:
+
+
+my_fav_bike='street triple'
+
+
+# In[56]:
+
+
+print(my_fav_bike)
+
+
+# In[57]:
+
+
+print(my_fav_bike.lstrip())
+
+
+# In[58]:
+
+
+print(my_fav_bike.rstrip())
+
+
+# In[59]:
+
+
+3+7
+
+
+# In[61]:
+
+
+3-7
+
+
+# In[62]:
+
+
+3*7
+
+
+# In[63]:
+
+
+3**7
+
+
+# In[64]:
+
+
+r=3**7
+
+
+# In[65]:
+
+
+type(r)
+
+
+# In[66]:
+
+
+k=3.7
+
+
+# In[67]:
+
+
+type(k)
+
+
+# In[68]:
+
+
+x,y,z=1,2,3
+
+
+# In[69]:
+
+
+print(x,y,z)
+
+
+# In[70]:
+
+
+list=['abc',1,5.5]
+
+
+# In[71]:
+
+
+print(list)
+
+
+# In[76]:
+
+
+k=['a',1,2,r]
+
+
+# In[77]:
+
+
+r=['b',4,5,k]
+
+
+# In[78]:
+
+
+print(k)
+print(r)
+
+
+# In[79]:
+
+
+i=['a',1,2,3]
+
+
+# In[81]:
+
+
+i[1]='h'
+
+
+# In[82]:
+
+
+print(i)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
